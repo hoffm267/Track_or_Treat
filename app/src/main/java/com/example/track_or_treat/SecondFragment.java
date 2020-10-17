2f4fa90877548dfdeddef78e57bc8ba07662fd55
hoffm267 <hoffm267@purdue.edu>
2020-10-17 14:51:48 -0400

    inputs complete

--- app/src/main/java/com/example/track_or_treat/SecondFragment.java
+++ app/src/main/java/com/example/track_or_treat/SecondFragment.java
@@ -4,14 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.fragment.app.FragmentManager;
 
 public class SecondFragment extends Fragment {
+    public String location;
+    public int age, duration;
+    public boolean driving;
 
-    @Override
+    EditText ageInput;
+    EditText locationsInput;
+    EditText durationInput;
+    EditText drivingInput;
+
+    Button button;
+
+
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
@@ -23,12 +37,33 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        ageInput = (EditText) ((MainActivity) getActivity()).findViewById(R.id.ageInput);
+        locationsInput = (EditText) ((MainActivity) getActivity()).findViewById(R.id.locationInput);
+        durationInput = (EditText) ((MainActivity) getActivity()).findViewById(R.id.durationInput);
+        drivingInput = (EditText) ((MainActivity) getActivity()).findViewById(R.id.drivingInput);
+
+
+
+        button = (Button) ((MainActivity) getActivity()).findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            public void onClick(View v) {
+                age = Integer.valueOf(ageInput.getText().toString());
+                location = locationsInput.getText().toString();
+                duration = Integer.valueOf(durationInput.getText().toString());
+                if (drivingInput.getText().toString().toLowerCase() == "yes") {
+                    driving = true;
+                } else {
+                    driving = false;
+                }
+
             }
         });
+
     }
 }
+
+
+
+
+
